a26134f33a8b42c31776bc13b3f8bed00fafe15e
abdulkaeum <abdul.kaeum@hotmail.com>
2021-12-21 11:44:16 +0000

    start dashboard styling

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,29 +1,34 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Project Dashboard') }}
-        </h2>
+        <div class="flex justify-between w-full items-end">
+            <h2 class="font-semibold text-lg text-gray-800 leading-tight">
+                {{ __('Project Dashboard') }}
+            </h2>
+
+            <a href="" class="btn-primary">New Projects</a>
+        </div>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-
-                    <div class="flex justify-between mb-4">
-                        <h1 class="text-lg">My Projects</h1>
-                        <a href="/projects/create">Create</a>
-                    </div>
-
-                    <ul>
+            <div class="overflow-hidden">
+                <div class="p-6">
+                    <div class="lg:flex lg:flex-wrap -mx-3">
                         @forelse($projects as $project)
-                            <li>
-                                <a href="{{ $project->path() }}">{{ $project->title }}</a>
-                            </li>
+                            <div class="lg:w-1/3 px-3 pb-6">
+                                <a href="{{ $project->path() }}">
+                                    <div class="bg-white p-5 rounded-lg shadow" style="height: 200px">
+                                        <h3 class="text-xl mb-6 py-4 -ml-5 border-l-4 border-cyan-400 pl-4">{{ $project->title }}</h3>
+                                        <div
+                                            class="text-sm text-gray-500">{{ Str::limit($project->description, 100) }}
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
                         @empty
-                            <h2>No projetcs created yet</h2>
+                            <div>No projetcs created yet</div>
                         @endforelse
-                    </ul>
+                    </div>
                 </div>
             </div>
         </div>
