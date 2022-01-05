8e272fd902bd13cff470a375e01839545a4d927b
abdulkaeum <abdul.kaeum@hotmail.com>
2022-01-05 22:00:08 +0000

    projects routes have the 7 restful routes: move to Route::resource now

--- routes/web.php
+++ routes/web.php
@@ -26,13 +26,14 @@ Route::get('/dashboard', function () {
 require __DIR__.'/auth.php';
 
 Route::group(['middleware' => 'auth'], function(){
-    Route::get('projects', [ProjectsController::class, 'index'])->name('projects.index');
+    /*Route::get('projects', [ProjectsController::class, 'index'])->name('projects.index');
     Route::get('projects/create', [ProjectsController::class, 'create']);
     Route::get('projects/{project}', [ProjectsController::class, 'show']);
     Route::get('projects/{project}/edit', [ProjectsController::class, 'edit']);
     Route::patch('projects/{project}', [ProjectsController::class, 'update']);
     Route::post('projects', [ProjectsController::class, 'store']);
-    Route::delete('projects/{project}', [ProjectsController::class, 'destroy']);
+    Route::delete('projects/{project}', [ProjectsController::class, 'destroy']);*/
+    Route::resource('projects', ProjectsController::class);
 
     Route::post('projects/{project}/tasks', [ProjectsTasksController::class, 'store']);
     Route::patch('projects/{project}/tasks/{task}', [ProjectsTasksController::class, 'update']);
